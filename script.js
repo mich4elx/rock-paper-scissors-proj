2d3ccda54a4895f747d9788a979c66f1483608a4
Michael Martinez <michaelm47x@gmail.com>
2022-05-03 12:46:30 -0500

    Changed switch statements to if/else statements for win/lose logic

--- script.js
+++ script.js
@@ -14,13 +14,20 @@ const playerChoice = prompt("Rock, Paper, or Scissors?").toLowerCase();
 
 // Function to start game and accept two parameters for player and computer choices.
 function playGame(playerChoice, computerChoice){
-    switch (playerChoice){
-        case playerChoice === computerChoice:
-            console.log("You tied the CPU!");
-            break;
-        case playerChoice === 'rock' && computerChoice === 'paper':
-            console.log("You won!")
-            break;
+    if(playerChoice === computerChoice){
+        console.log("You tied!")
+    }else if(playerChoice === 'rock' && computerChoice === 'paper'){
+        console.log("You lost! Paper covers Rock!")
+    }else if(playerChoice === 'rock' && computerChoice === 'scissors'){
+        console.log("You won! Rock smashes Scissors!")
+    }else if(playerChoice === 'paper' && computerChoice === 'rock'){
+        console.log("You won! Paper covers Rock!")
+    }else if(playerChoice === 'paper' && computerChoice === 'scissors'){
+        console.log("You lost! Scissors cuts Paper!")
+    }else if(playerChoice === 'scissors' && computerChoice === 'rock'){
+        console.log("You lost! Rock smashes Scissors!")
+    }else if(playerChoice === 'scissors' && computerChoice === 'paper'){
+        console.log("You won! Scissors cuts Paper!")
     }
     
 }
